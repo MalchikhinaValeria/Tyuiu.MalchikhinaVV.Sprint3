0f67f3227ae373448a1486be246f30f6161ae1af
Home <Home@LAPTOP-L43VJUUA>
2025-10-26 15:18:30 +0500

    Add project files.

---
+++ Tyuiu.MalchikhinaVV.Sprint3.Task0.V8.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.MalchikhinaVV.Sprint3.Task0.V8.Lib
+{
+    public class DataService : ISprint3Task0V8
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double mulSeries = 1;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                mulSeries = mulSeries * (Math.Pow(value, i) + 1);
+            }
+            mulSeries = mulSeries * Math.Cos(4);
+            return Math.Round(mulSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.MalchikhinaVV.Sprint3.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint3.Task0.V8.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint3.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 10;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = -0.886;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
