ce5592f5f5dc8a22429fe835a0eb65f7b76e5119
Home <Home@LAPTOP-L43VJUUA>
2025-10-26 21:00:11 +0500

    Task2

---
+++ Tyuiu.MalchikhinaVV.Sprint3.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint3.Task2.V26.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint3.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 17;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 411589.537;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
