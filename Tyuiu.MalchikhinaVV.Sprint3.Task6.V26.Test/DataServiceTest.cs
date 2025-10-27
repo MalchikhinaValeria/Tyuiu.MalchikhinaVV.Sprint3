7a5d8f946fdf9dfe4b4051908a875f3ec2a61fd5
Home <Home@LAPTOP-L43VJUUA>
2025-10-27 19:40:05 +0500

    Task6

---
+++ Tyuiu.MalchikhinaVV.Sprint3.Task6.V26.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MalchikhinaVV.Sprint3.Task6.V26.Lib;
+namespace Tyuiu.MalchikhinaVV.Sprint3.Task6.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 15;
+            int stopValue = 22;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 33;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
